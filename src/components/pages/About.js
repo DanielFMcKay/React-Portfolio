4cd3cc6e968a84f3afea3b113222fa9e1e63c7cb
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-07-25 23:58:38 -0700

    init heroku commit again for the 50th time

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+                        Hi, my name is Dan McKay. I am a native of Washington DC who has lived in
+                        the Bay Area for over 10 years now, and have extensive experience in the non-profit
+                        sector helping run the Meals on Wheels program at <u><a href="https://vivalon.org"
+                        target="_blank">Vivalon</a></u> in Marin County. Currently I am in the middle
+                        of a giant course change in my life.
+                    </p>
+    </div>
+  );
+}
